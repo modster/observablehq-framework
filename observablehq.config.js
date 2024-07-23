fc90cc52b019a16098056e7d6fb18341d9ae2060
EM Greeff <emgreeff@datadash.cloud>
2024-07-23 08:06:20 -0600

    On branch master Changes to be committed: 	modified:   observablehq.config.js 	new file:   src/generator.js 	modified:   src/index.md

--- observablehq.config.js
+++ observablehq.config.js
@@ -23,9 +23,9 @@ export default {
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
+  theme: "dark",// "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
--- src/generator.js
+++ src/generator.js
@@ -1,6 +1,4 @@
-# BTC
 
-```js
 const wssUrl = "wss://ws.eodhistoricaldata.com/ws/crypto?api_token=demo";
 const socket = new WebSocket(wssUrl);
 invalidation.then(() => socket.close());
@@ -24,15 +22,7 @@ const messages = Generators.observe((change) => {
   return () => socket.removeEventListener("message", messaged);
 
 });
-```
 
-## 60 seconds
 
-```js
-Plot.plot({
-  marginLeft: 50,
-  x: {type: "time", domain: [now - messages.duration, now]},
-  y: {type: "linear", label: "price", inset: 10},
-  marks: [Plot.lineY(messages, {x: "t", y: "p", curve: "step", clip: true})]
-})
-```
+
+
